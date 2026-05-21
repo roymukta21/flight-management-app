e1026b06ead65b7373753b8a31c98bfd2f08ba81
MUKTA ROY <muktaroy520@gmail.com>
2026-05-21 22:17:41 +0600

    Implement booking functionality in BookingForm component with form handling and state management

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "@/lib/supabase";
 
 type BookingFormProps = {
   selectedSeat: string;
 };
 
-const BookingForm = ({ selectedSeat }: BookingFormProps) => {
+const BookingForm = ({
+  selectedSeat,
+}: BookingFormProps) => {
+  const [fullName, setFullName] = useState("");
+  const [passportNo, setPassportNo] = useState("");
+  const [nationality, setNationality] = useState("");
+
+  const handleBooking = async (
+    e: React.FormEvent
+  ) => {
+    e.preventDefault();
+
+    const { error } = await supabase
+      .from("passengers")
+      .insert([
+        {
+          full_name: fullName,
+          passport_no: passportNo,
+          nationality: nationality,
+        },
+      ]);
+
+    if (error) {
+      alert("Booking failed");
+      return;
+    }
+
+    alert("Booking successful");
+  };
+
   return (
     <div className="mt-6 bg-card border border-zinc-800 rounded-2xl p-5">
-      <p className="text-muted text-sm">Selected Seat</p>
+      <p className="text-muted text-sm">
+        Selected Seat
+      </p>
+
       <h2 className="text-2xl font-bold text-primary mb-5">
         {selectedSeat}
       </h2>
@@ -18,22 +51,37 @@ const BookingForm = ({ selectedSeat }: BookingFormProps) => {
         Passenger Information
       </h3>
 
-      <form className="space-y-4">
+      <form
+        onSubmit={handleBooking}
+        className="space-y-4"
+      >
         <input
           type="text"
           placeholder="Full Name"
+          value={fullName}
+          onChange={(e) =>
+            setFullName(e.target.value)
+          }
           className="w-full bg-secondary border border-zinc-700 rounded-xl px-4 py-3 outline-none"
         />
 
         <input
           type="text"
           placeholder="Passport Number"
+          value={passportNo}
+          onChange={(e) =>
+            setPassportNo(e.target.value)
+          }
           className="w-full bg-secondary border border-zinc-700 rounded-xl px-4 py-3 outline-none"
         />
 
         <input
           type="text"
           placeholder="Nationality"
+          value={nationality}
+          onChange={(e) =>
+            setNationality(e.target.value)
+          }
           className="w-full bg-secondary border border-zinc-700 rounded-xl px-4 py-3 outline-none"
         />
 
@@ -42,7 +90,6 @@ const BookingForm = ({ selectedSeat }: BookingFormProps) => {
         </button>
       </form>
     </div>
-    
   );
 };
 
